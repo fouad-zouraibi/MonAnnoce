beb14246d5b4a0bc5b67803166f37348d90a8b55
fouadzrb <f.zouraibi@gmail.com>
2022-11-08 11:27:13 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,44 @@
 import data from "./data";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 
 
 function App() {
-  return (
-    <div>
-      <header>
-          <a href="/">MonAnnonace</a>
-      </header>
-        <main>
-            <h1>Featured Products</h1>
-            {
-                data.products.map(product => (
-                    <div>
-                        <img src={product.image} alt={product.name}/>
-                        <p>{product.name}</p>
-                        <p>{product.price}</p>
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <a href="/">MonAnnonace</a>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} />
+                    </Routes>
+                    <h1>Featured Products</h1>
+                    <div className="products">
+                        {
+                            data.products.map(product => (
+                                <div className="product" key={product.slug}>
+                                    <a href={'/product/${product.slug}'}>
+                                        <img src={product.image} alt={product.name}/>
+                                    </a>
+                                    <div className="product-info">
+                                        <a href={'/product/${product.slug}'}>
+                                            <p>{product.name}</p>
+                                        </a>
+                                        <a href={'/product/${product.slug}'}>
+                                            <p><strong>€{product.price}</strong></p>
+                                        </a>
+
+                                        <button>Add to cart</button>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
-                ))
-            }
-        </main>
-    </div>
-  );
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;

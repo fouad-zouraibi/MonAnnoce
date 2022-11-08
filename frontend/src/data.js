dfc2f20b1e8ba036c6888b7199bf258ee89d67c6
Fouad Zouraibi <f.zouraibi@gmail.com>
2022-11-08 10:02:49 +0100

    data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,56 +1,56 @@
 const data = {
-    products: = [
+    products: [
         {
             name: 'Levis Jeans',
             slug: 'Levis-Jeans',
             category: 'Cloths',
             image: '/images/p1.jpg',
-            price: 120€,
+            price: 120,
             countInStock: 20,
             brand: 'Levis',
             rating: 4.5,
             numReviews: 1000,
             description: 'High quality jeans. ',
-        }
+        },
         {
             name: 'Table de nuits',
             slug: 'IKEA',
             category: 'Meubles',
             image: '/images/p2.jpg',
-            price: 20€,
+            price: 20,
             countInStock: 2,
             brand: 'IKEA',
             adresse: '60 rue albert, 70020 '
             rating: 4.5,
             numReviews: 1500,
             description: 'Table de nuits occassion comme neuf ',
-        }
+        },
         {
             name: 'Studio T2',
             slug: 'Studio-T2',
             category: 'Maison',
             image: '/images/p3.jpg',
-            price: 750€,
-            surface: '25m2',
+            price: 750,
+            surface: 25,
             Adresse: '76 rue de meaux, 70020',
             rating: 4.5,
             numReviews: 1200,
             description: 'Studio T2 à 25m2, tous les charges comprise. ',
-        }
+        },
         {
             name: 'Ford Focus',
             slug: 'Ford-Focus',
             category: 'Voitue',
             image: '/images/p4.jpg',
-            price: 5500€,
+            price: 5500,
             countInStock: 1,
             brand: 'Ford',
             model: '2015'
-            kilometrage: '120 000 km',
+            kilometrage: 120000,
             adresse: 'Versailles 78000',
             rating: 4,
-            numReviews: 1000,
+            numReviews: 2000,
             description: 'Voiture Occassion etats bonne toutes options model 2015 ',
-        }
-    ]
-}
+        },
+    ],
+};
